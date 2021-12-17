0cad48625ea9e6aef6243a011460671fa03d6f8d
Debarghya Datta <debarghya.datta89@gmail.com>
2021-12-17 21:57:00 +0530

    Added basic files

---
+++ .config/dwm/bar.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# ^c$var^ = fg color
+# ^b$var^ = bg color
+
+interval=0
+
+# load colors!
+. ~/.config/dwm/themes/onedark
+
+cpu() {
+	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+
+	printf "^c$black^ ^b$green^  "
+	printf "^c$white^ ^b$grey^ $cpu_val"
+}
+
+pkg_updates() {
+	# updates=$(doas xbps-install -un | wc -l) # void
+	updates=$(checkupdates | wc -l)   # arch , needs pacman contrib
+	# updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
+
+	printf "^c$green^  $updates"
+}
+
+battery() {
+	get_capacity="$(cat /sys/class/power_supply/BAT0/capacity)"
+	printf "^c$blue^   $get_capacity"
+}
+
+brightness() {
+	printf "^c$red^   "
+	printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
+}
+
+mem() {
+	printf "^c$blue^^b$black^  "
+	printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+}
+
+wlan() {
+	case "$(cat /sys/class/net/w*/operstate 2>/dev/null)" in
+	up) printf "^c$black^ ^b$blue^ 爵 ^d^%s" " ^c$blue^Connected" ;;
+	down) printf "^c$black^ ^b$blue^ 爵 ^d^%s" " ^c$blue^Disconnected" ;;
+	esac
+}
+
+clock() {
+	printf "^c$black^ ^b$darkblue^  "
+	printf "^c$black^^b$blue^ $(date '+%I:%M %p') "
+}
+
+while true; do
+
+	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+	interval=$((interval + 1))
+
+	sleep 1 && xsetroot -name "$updates $(battery) $(cpu) $(mem) $(wlan) $(clock)"
+done
